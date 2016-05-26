d3b56ca1ece70f5f2a33e41ff1d463cbf4592a67
Ashutosh Agrawal <ashutosh_agrawal@live.in>
2016-05-26 20:18:09 +0530

    Configuration laoder setup

--- lib/JIRA_Migrator.rb
+++ lib/JIRA_Migrator.rb
@@ -12,3 +12,6 @@ load_project 'lib/jira_migrator/config.rb'
 $config = JIRAMigrator::Config.new
 
 
+SOURCE_CLIENT = JIRA_Client.new("https://tinyowl.atlassian.net", "ashutosh:Ashu@2609")
+DESTINATION_CLIENT = JIRA_Client.new("https://roadrunnr.atlassian.net", "ashutosh.agrawal:Ashu@2609")
+
---
+++ lib/jira_migrator/rest_client.rb
@@ -0,0 +1,38 @@
+module JIRAMigrator
+  class RestClient
+
+    require 'restclient'
+    require 'base64'
+
+    def initialize(base_url, auth)
+
+    @base_url = base_url
+    @auth = Base64.encode64("#{auth}")
+
+    end
+
+    def get(url)
+      RestClient.get("#{@base_url}#{url}", {authorization: "Basic #{@auth}", content_type: :json })
+    end
+
+  end
+end
+
+
+class JIRA_Client
+  require 'restclient'
+  require 'base64'
+
+  def initialize(base_url, auth)
+
+    @base_url = base_url
+    @auth = Base64.encode64("#{auth}")
+
+  end
+
+  def get(url)
+    RestClient.get("#{@base_url}#{url}", {authorization: "Basic #{@auth}", content_type: :json })
+  end
+
+end
+
